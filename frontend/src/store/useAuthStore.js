c39c3a0128ead84bb71cc876e9554032378ddd78
Dominic Chong <119094525+dominicchong@users.noreply.github.com>
2025-11-12 22:53:16 +0800

    change to auth store url

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from 'react-hot-toast';
 import { io } from 'socket.io-client';
 import { useChatStore } from './useChatStore.js';
 
-const BASE_URL = import.meta.env.MODE === "development" ? import.meta.env.VITE_BACKEND_URL : "/";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
