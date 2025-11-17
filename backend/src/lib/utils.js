c4aebbdce7526897b7eae2833adeed8b7a262d60
Dominic Chong <119094525+dominicchong@users.noreply.github.com>
2025-11-17 11:24:12 +0800

    fix upload size for images

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -5,7 +5,7 @@ export const generateToken = (userId, res) => {
         expiresIn: '7d',
     });
     res.cookie('token', token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 30 days
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
         sameSite: 'strict',  // Helps prevent CSRF attacks
         secure: process.env.NODE_ENV !== 'development',
