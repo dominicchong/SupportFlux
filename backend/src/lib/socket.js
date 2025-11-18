3088d4b3b0bbd17e3afd6c5b0726df52559136cc
Dominic Chong <119094525+dominicchong@users.noreply.github.com>
2025-11-18 16:02:15 +0800

    url change and remove unused file

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,13 +1,15 @@
 import {Server} from 'socket.io';
 import http from 'http';
 import express from 'express';
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-      origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
   },
 });
 
