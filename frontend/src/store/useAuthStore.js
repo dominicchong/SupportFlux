3a9ae7677e2404ab2aecf61147011256310ea566
Dominic Chong <119094525+dominicchong@users.noreply.github.com>
2026-01-09 15:11:24 +0800

    Revert "URL to link from frontend to backend api"
    This reverts commit e90d3d270ec0bb5e80023a6484064ced00ecd62e.

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,7 +5,7 @@ import { io } from 'socket.io-client';
 import { useTicketStore } from './useTicketStore.js';
 import { useChatbotStore } from './useChatbotStore.js';
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
